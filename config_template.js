e668d93d3c4be7e62efb8b59a0b0c0a67b6d12fb
boomngong <boom_pangya_11@hotmail.com>
2018-05-06 21:50:18 +0700

    fix README.md

--- config_template.js
+++ config_template.js
@@ -1,7 +1,7 @@
-module.exports.collectionName = "fastcheck";
-module.exports.region = "ap-northeast-1";
-module.exports.bucket = "rekogpolly";
-module.exports.keyPath = "video/";
-module.exports.SNSArn = "arn:aws:sns:ap-northeast-1:661935917969:SNS-RekognitionVideo-Topic"
-module.exports.roleArn = "arn:aws:iam::661935917969:role/RekognitionVideo";
-module.exports.queueURL = "https://sqs.ap-northeast-1.amazonaws.com/661935917969/queueProject";
+module.exports.collectionName = "Your Rekognition's collection name";
+module.exports.region = "Your AWS region";
+module.exports.bucket = "Your S3 bucket name";
+module.exports.keyPath = "Your S3 bucket folder (if you have)";
+module.exports.SNSArn = "Your SNS Arn"
+module.exports.roleArn = "Your IAM role Arn";
+module.exports.queueURL = "Your SQS queue URL";
